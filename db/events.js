03884ff427cb03f2209d97146cd68402a3c0e0cc
Luiz Fernando <lfernando-silva@hotmail.com>
2018-06-10 06:30:12 -0300

    get all

--- db/events.js
+++ db/events.js
@@ -4,5 +4,6 @@ module.exports = {
     findById: (id) => knex().select().from('events').where({id}),
     update: (id, data) => knex().from('events').update(data).where({id}).returning('*'),
     insert: (data) => knex().insert(data).into('events').returning('*'),
-    remove: (id) => knex().from('events').where({id}).del()
+    remove: (id) => knex().from('events').where({id}).del(),
+    findAll: () => knex().select().from('events')
 };
--- routes/events.js
+++ routes/events.js
@@ -5,7 +5,7 @@ const {events} = require('../db')
 
 router.get('/:id', async (req, res) => {
     try {
-        const { id } = req.params;
+        const {id} = req.params;
         const event = await events.findById(id);
         return res.status(200).json(event);
     } catch (err) {
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        const { id } = req.params;
+        const {id} = req.params;
         const event = await events.remove(id);
         return res.status(200).json(event);
     } catch (err) {
@@ -31,8 +31,8 @@ router.delete('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const { lat, long, date, crime } = req.body;
-        const event = await events.insert({ lat, long, date, crime });
+        const {lat, long, date, crime} = req.body;
+        const event = await events.insert({lat, long, date, crime});
         return res.status(200).json(event);
     } catch (err) {
         return res.status(500).json({
@@ -44,9 +44,21 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     try {
-        const { id } = req.params;
-        const { lat, long, date, crime } = req.body;
-        const event = await events.update(id, { lat, long, date, crime });
+        const {id} = req.params;
+        const {lat, long, date, crime} = req.body;
+        const event = await events.update(id, {lat, long, date, crime});
+        return res.status(200).json(event);
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error',
+            stack: err
+        });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const event = await events.findAll();
         return res.status(200).json(event);
     } catch (err) {
         return res.status(500).json({
